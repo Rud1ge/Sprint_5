66da3eb6686f143e1d1c5eb6d7b5d627ddb43b3a
Руди Герман Геннадьевич <rudige@yandex.ru>
2026-06-15 22:47:38 +0700

    feat: add locators to the first task

---
+++ tests/locators.py
@@ -0,0 +1,7 @@
+# Регистрация пользователя
+BUTTON_LOGIN_REGISTRATION = "/html/body/div/div/div[1]/div/button"
+BUTTON_NO_ACCOUNT = "/html/body/div/div/div[2]/div[5]/form/div[3]/button[2]"
+BUTTON_CREATE_ACCOUNT = "/html/body/div/div/div[2]/div[5]/form/div[3]/button[1]"
+BUTTON_PROFILE = "/html/body/div/div/div[1]/div/div[1]/button"
+FIELD_EMAIL = "/html/body/div/div/div[2]/div[5]/form/div[2]/div[1]/div/div/input"
+USERNAME = "/html/body/div/div/div[1]/div/div[1]/div/h3"

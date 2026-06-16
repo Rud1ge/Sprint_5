6858655308bebbcb881eb7bb5364cb9467a01a06
Руди Герман Геннадьевич <german.rudi@eltex.loc>
2026-06-16 15:39:08 +0700

    chore: unify comment punctuation in tests

--- tests/test_user_registration_existing_user.py
+++ tests/test_user_registration_existing_user.py
@@ -18,17 +18,17 @@ from tests.locators import (
 
 def test_user_registration_existing_user(driver, existing_user_credentials):
     existing_email, existing_password = existing_user_credentials
-    # Нажать кнопку «Вход и регистрация»
+    # Нажать кнопку «Вход и регистрация».
     driver.find_element(By.XPATH, BUTTON_LOGIN_REGISTRATION).click()
-    # Нажать кнопку «Нет аккаунта»
+    # Нажать кнопку «Нет аккаунта».
     driver.find_element(By.XPATH, BUTTON_NO_ACCOUNT).click()
-    # Заполнить все поля формы регистрации данными уже существующего пользователя и нажать «Создать аккаунт»
+    # Заполнить все поля формы регистрации данными уже существующего пользователя и нажать «Создать аккаунт».
     driver.find_element(By.XPATH, FIELD_EMAIL).send_keys(existing_email)
     driver.find_element(By.XPATH, FIELD_PASSWORD).send_keys(existing_password)
     driver.find_element(By.XPATH, FIELD_REPEAT_PASSWORD).send_keys(existing_password)
     driver.find_element(By.XPATH, BUTTON_CREATE_ACCOUNT).click()
 
-    # Проверить: поля Email, «Пароль», «Повторите пароль» выделены красным, под полем Email отображается сообщение «Ошибка»
+    # Проверить: поля Email, «Пароль», «Повторите пароль» выделены красным, под полем Email отображается сообщение «Ошибка».
     WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, FIELD_ERROR_EMAIL)))
     assert driver.find_element(By.XPATH, FIELD_ERROR_PASSWORD)
     assert driver.find_element(By.XPATH, FIELD_ERROR_REPEAT_PASSWORD)
--- tests/test_user_registration_not_by_mask.py
+++ tests/test_user_registration_not_by_mask.py
@@ -17,15 +17,15 @@ from tests.locators import (
 
 
 def test_user_registration_not_by_mask(driver):
-    # Нажать кнопку «Вход и регистрация»
+    # Нажать кнопку «Вход и регистрация».
     driver.find_element(By.XPATH, BUTTON_LOGIN_REGISTRATION).click()
-    # Нажать кнопку «Нет аккаунта»
+    # Нажать кнопку «Нет аккаунта».
     driver.find_element(By.XPATH, BUTTON_NO_ACCOUNT).click()
-    # Заполнить поле Email формы регистрации и нажать кнопку «Создать аккаунт»
+    # Заполнить поле Email формы регистрации и нажать кнопку «Создать аккаунт».
     driver.find_element(By.XPATH, FIELD_EMAIL).send_keys(f"{uuid4()}")
     driver.find_element(By.XPATH, BUTTON_CREATE_ACCOUNT).click()
 
-    # Проверить: поля Email, «Пароль», «Повторите пароль» выделены красным, под полем Email отображается сообщение «Ошибка»
+    # Проверить: поля Email, «Пароль», «Повторите пароль» выделены красным, под полем Email отображается сообщение «Ошибка».
     WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, FIELD_ERROR_EMAIL)))
     assert driver.find_element(By.XPATH, FIELD_ERROR_PASSWORD)
     assert driver.find_element(By.XPATH, FIELD_ERROR_REPEAT_PASSWORD)
